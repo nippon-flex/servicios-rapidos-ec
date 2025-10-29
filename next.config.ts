cfbc36c447c7eae5d6dad49337b35387fe7ea0d0
rafael Millan <millanr231@gmail.com>
2025-10-29 16:01:26 -0500

    fix: corregir sintaxis Next.js 16 para Prisma

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-    outputFileTracingIncludes: {
-      '/**/*': ['./node_modules/.prisma/client/libquery_engine-*.so.node'],
-    },
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
+  serverExternalPackages: ['@prisma/client', 'prisma'],
+  outputFileTracingIncludes: {
+    '/**/*': ['./node_modules/.prisma/client/libquery_engine-*.so.node'],
   },
 };
 
