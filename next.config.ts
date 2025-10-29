d2b579ef5fb54c5f9edd2c94773e3c686fa5aacb
rafael Millan <millanr231@gmail.com>
2025-10-29 15:55:10 -0500

    fix: configuración agresiva outputFileTracing para Prisma

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+    outputFileTracingIncludes: {
+      '/**/*': ['./node_modules/.prisma/client/libquery_engine-*.so.node'],
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
   },
 };
 
